7f2825c3afd9931d686cc102bb1845d1888061cb
AntRed <https://github.com/hoangtan23/Frontend>
2023-08-30 00:43:38 +0700

    upfile2908

--- src/Layout/Profile/index.js
+++ src/Layout/Profile/index.js
@@ -1,17 +1,56 @@
 import styles from './Profile.module.scss';
 import className from 'classnames/bind';
+import Slider from 'react-slick';
+import { Link } from 'react-router-dom';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import { project, landingpages } from '../../images/index.js';
 
 const cx = className.bind(styles);
 
 function Profile() {
+    var settings = {
+        dots: true,
+        className: `${styles.slickslider}`,
+        dotsClass: `slick-dots ${styles.dots}`,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        autoplay: false,
+        autoplaySpeed: 2000,
+    };
+
     return (
         <div className={cx('wrapper')}>
             <header>Header</header>
             <div className={cx('content')}>
                 <h3>Project</h3>
-                <div>Content</div>
+
+                <Slider {...settings}>
+                    {project.map((item, index) => (
+                        <div className={cx('content-item')} key={index}>
+                            <img src={item.img} />
+                            <div>{item.tiltle}</div>
+                            <Link className={cx('btn')}>view</Link>
+                        </div>
+                    ))}
+                </Slider>
+
                 <h3>Landing Page</h3>
-                <div>Content</div>
+
+                <Slider {...settings}>
+                    {landingpages.map((item, index) => (
+                        <div className={cx('content-item')} key={index}>
+                            <img src={item.img} />
+                            <div>{item.tiltle}</div>
+                            <Link className={cx('btn')}>view</Link>
+                        </div>
+                    ))}
+                </Slider>
+
                 <button>CV</button>
             </div>
         </div>
---
+++ src/images/index.js
@@ -0,0 +1,55 @@
+import VCB from './VCB.png';
+import rainymood from './rainymood.png';
+import CarZone from './CarZone.png';
+import influexa from './Influexa.png';
+import Spinfluence from './Spinfluence.png';
+import UXUI_Desogner from './UXUI_Desogner.png';
+import Warkinon from './Warkinon.png';
+
+export const project = [
+    {
+        id: 0,
+        img: VCB,
+        tiltle: 'VCB Internet Banking',
+        router: '/VCB',
+    },
+    {
+        id: 1,
+        img: rainymood,
+        tiltle: 'Rainy Mood',
+        router: '/rainymood',
+    },
+];
+
+export const landingpages = [
+    {
+        id: 0,
+        img: CarZone,
+        tiltle: 'Car Zone',
+        router: '/CarZone',
+    },
+    {
+        id: 1,
+        img: influexa,
+        tiltle: 'Influexa',
+        router: '/influexa',
+    },
+    {
+        id: 2,
+        img: Spinfluence,
+        tiltle: 'Spin Fluence',
+        router: '/Spinfluence',
+    },
+    {
+        id: 3,
+        img: UXUI_Desogner,
+        tiltle: 'UXUI Desogner',
+        router: '/UXUI_Desogner',
+    },
+    {
+        id: 4,
+        img: Warkinon,
+        tiltle: 'Warkinon',
+        router: '/Warkinon',
+    },
+];
